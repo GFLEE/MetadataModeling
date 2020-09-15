0a40b45029394a38f0473384566102a131d6e412
e <leegradylll@gmail.com>
2020-09-15 17:51:52 +0800

    finish ColumnXml

---
+++ Common/MetadataModeling.Wpf/NotifyPropertyChangedBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+
+namespace MetadataModeling.Wpf
+{
+    /// <summary>
+    /// NotifyPropertyChangedBase
+    /// </summary>
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 引发属性更改
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public  void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+            }
+            OnNotifyPropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// 当属性变更后，需要执行的方法
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnNotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null)
+            {
+                return;
+            }
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+    }
+}
--- MetadataModeling.Base/Database/Xml/ColumnXml.cs
+++ MetadataModeling.Base/Database/Xml/ColumnXml.cs
@@ -43,7 +43,98 @@ namespace MetadataModeling.Base.Database.Xml
         [DataMember]
         public string RefFkColumnID { get; set; }
 
+        /// <summary>
+        /// 长度(字符类型有效)
+        /// </summary>
+        [DataMember]
+        public int Length { get; set; }
+
+        /// <summary>
+        /// 允许Null
+        /// </summary>
+        [DataMember]
+        public bool IsNull { get; set; }
+
+        /// <summary>
+        /// 必填
+        /// </summary>
+        [DataMember]
+        public bool IsRequired { get; set; }
 
 
+        /// <summary>
+        /// 精度(decimal)
+        /// </summary>
+        [DataMember]
+        public int Prec { get; set; }
+
+        /// <summary>
+        /// 小数位数(decimal)
+        /// </summary>
+        [DataMember]
+        public int Scale { get; set; }
+
+        /// <summary>
+        /// 自增标识(Int)
+        /// </summary>
+        [DataMember]
+        public bool IsIdentity{ get; set; }
+
+
+        /// <summary>
+        /// 顺序(主键应该永远在第一位)
+        /// </summary>
+        [DataMember]
+        public string DisplayIndex { get; set; }
+
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        [DataMember]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 字段对应长名称（出现在编辑界面）
+        /// </summary>
+        [DataMember]
+        public string LabelName { get; set; }
+
+        /// <summary>
+        /// 字段对应短名称（出现在列头界面）
+        /// </summary>
+        [DataMember]
+        public string LabelShortName { get; set; }
+
+
+        /// <summary>
+        /// 字段补充说明
+        /// </summary>
+        [DataMember]
+        public string LabelTooltips { get; set; }
+
+        /// <summary>
+        /// 分析代码
+        /// </summary>
+        [DataMember]
+        public string ACodeID1 { get; set; }
+
+        /// <summary>
+        /// 如果当前列引用系统列，此处为引用列信息
+        /// </summary>
+        [DataMember]
+        public string DefaultColumnID { get; set; }
+
+        /// <summary>
+        /// 如果字段是枚举，枚举代码
+        /// </summary>
+        [DataMember]
+        public string EnumCode { get; set; }
+         
+        /// <summary>
+        /// 引用其他数据库列的ID
+        /// </summary>
+        [DataMember]
+        public string RefOtherDbFkColumnID { get; set; }
     }
 }
