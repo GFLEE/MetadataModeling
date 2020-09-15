de139ffdbf31f223030d7181ac72ed4ef10f3016
e <leegradylll@gmail.com>
2020-09-15 10:58:19 +0800

    add dirs

---
+++ MetadataModeling.Base/Other/Copy/CopyData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetadataModeling.Base
+{
+    [Serializable]
+    public class CopyData
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
+ 
---
+++ MetadataModeling.Base/Other/Copy/ICopy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetadataModeling.Base
+{
+    public interface ICopy
+    {
+        /// <summary>
+        /// GetCopyDatas
+        /// </summary>
+        /// <returns></returns>
+        List<CopyData> GetCopyDatas();
+    }
+}
